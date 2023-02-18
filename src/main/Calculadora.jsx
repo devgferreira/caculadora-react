4899cc6f35a83c45644272465228b4a0e78ca2c6
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-02-17 22:34:27 -0300

    feat: criando a base da calculadora

---
+++ src/main/Calculadora.jsx
@@ -0,0 +1,11 @@
+import React,{ Component } from 'react';
+import './Calculadora.css'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default class Calculadora extends Component{
+    render(){
+        return(
+            <div className="Calculadora"></div>
+        )      
+    }
+}
