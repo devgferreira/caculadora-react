c030cbfbde10a9825439a35e30721833dd43a852
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-02-17 23:15:43 -0300

    feat: criando e separando a parte do button

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import './Button.css'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    
+    
+    return(
+            <button className='Button'>{props.label}</button>
+    )
+}
--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -1,11 +1,30 @@
 import React,{ Component } from 'react';
 import './Calculadora.css'
-
+import Button from '../components/Button';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default class Calculadora extends Component{
     render(){
         return(
-            <div className="Calculadora"></div>
+            <div className="Calculadora">
+              <Button label="AC"/>
+              <Button label="/"/>
+              <Button label="7"/>
+              <Button label="8"/>
+              <Button label="9"/>
+              <Button label="*"/>
+              <Button label="4"/>
+              <Button label="5"/>
+              <Button label="6"/>
+              <Button label="-"/>
+              <Button label="1"/>
+              <Button label="2"/>
+              <Button label="3"/>
+              <Button label="+"/>
+              <Button label="0"/>
+              <Button label="."/>
+              <Button label="="/>
+
+            </div>
         )      
     }
 }
