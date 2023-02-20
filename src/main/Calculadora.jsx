02b4ebc43e8d25bf5659910373963229dc1f7a26
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-02-20 14:16:54 -0300

    feat: adicionando a logica

--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -2,50 +2,97 @@ import React,{ Component } from 'react';
 import './Calculadora.css'
 import Button from '../components/Button';
 import Display from '../components/Display';
+
+
+    const initialState = {
+        displayValue: '0',
+        clearDisplay: false,
+        operation: null,
+        values: [0,0],
+        current: 0
+    }
+    
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default class Calculadora extends Component{
+
+    state = {...initialState}
     
     constructor(props){
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
-        this.setOperatiom = this.setOperatiom.bind(this)
+        this.setOperation = this.setOperation.bind(this)
         this.addDigit = this.addDigit.bind(this)
 
     }
 
     clearMemory(){
-        console.log('Limpar')
+        this.setState({...initialState})
     }
 
-    setOperatiom(operation){
-        console.log(operation)
+    setOperation(operation){
+       if(this.state.current === 0){
+            this.setState({operation, current: 1, clearDisplay: true})
+       }else{
+            const equals = operation === '='
+            const currenteOperation = this.state.operation
+            const values = [...this.state.values]
+
+            try{
+                // eslint-disable-next-line no-eval
+                values[0] = eval(`${values[0]} ${currenteOperation} ${values[1]}`)
+            }catch{
+                values[0] = this.state.values[0]
+            }           
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+       }
     }
 
     addDigit(n){
-        console.log(n)
+        if(n === '.' & this.state.displayValue.includes('.')){
+            return
+        }
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({displayValue, clearDisplay: false})
+        if(n !== '.'){
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)   
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({values})
+        }
     }
     render(){
-        const setOperatiom = operation => this.setOperatiom(operation)
         return(
             <div className="Calculadora">
-                <Display value={100}/>
+                <Display value={this.state.displayValue}/>
                 <Button label="AC" click={this.clearMemory} triple/>
-                <Button label="/" click={setOperatiom} operation/>
+                <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit} />
                 <Button label="8" click={this.addDigit} />
                 <Button label="9" click={this.addDigit}/>
-                <Button label="*" click={setOperatiom} operation/>
+                <Button label="*" click={this.setOperation} operation/>
                 <Button label="4" click={this.addDigit}/>
                 <Button label="5" click={this.addDigit}/>
                 <Button label="6" click={this.addDigit}/>
-                <Button label="-"click={setOperatiom} operation/>
+                <Button label="-" click={this.setOperation} operation/>
                 <Button label="1" click={this.addDigit}/>
                 <Button label="2" click={this.addDigit}/>
                 <Button label="3" click={this.addDigit}/>
-                <Button label="+"click={setOperatiom} operation/>
-                <Button label="0"click={this.addDigit} double/>
-                <Button label="." />
-                <Button label="="/>
+                <Button label="+" click={this.setOperation} operation/>
+                <Button label="0" click={this.addDigit} double/>
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} operation/>
 
             </div>
         )      
